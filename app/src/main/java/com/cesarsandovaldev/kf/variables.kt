a83b1147f08aed31f06dba0263bafa494bf96f94
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-02-25 22:53:53 -0300

    variables alfanumericas

--- app/src/main/java/com/cesarsandovaldev/kf/variables.kt
+++ app/src/main/java/com/cesarsandovaldev/kf/variables.kt
@@ -6,17 +6,24 @@ fun main() {
 
 
 //VARIABLES NUMERICAS
-
     //Integer
     val integerExample:Int = 42
-
     //Long
     val longExample:Long = 45568787676
-
     //Float
     val floatExample:Float = 3.14f
-
     //Double
     val doubleExample:Double = 3.14159265359
+
+
+//VARIABLES AlFANUMERICAS
+    //Char
+    val charExample:Char = '@'
+    //String
+    val stringExample:String = "esto es un String"
+
+//VARIABLES BOOLEANA
+    val booleanExample:Boolean = true
+    val booleanExample2:Boolean = false
 
 }
