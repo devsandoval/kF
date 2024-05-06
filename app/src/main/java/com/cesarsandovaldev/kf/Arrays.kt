9e320f6982a6ddc26eb2d814eb5ef1f53577e662
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-05-06 19:23:15 -0400

    arrays

--- app/src/main/java/com/cesarsandovaldev/kf/Arrays.kt
+++ app/src/main/java/com/cesarsandovaldev/kf/Arrays.kt
@@ -1,2 +1,22 @@
 package com.cesarsandovaldev.kf
 
+import kotlin.math.sin
+
+fun main() {
+    val diasSemanales =
+        arrayOf("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+
+    println(diasSemanales[0])
+
+    println(diasSemanales.size)
+
+    if (diasSemanales.size >= 8) {
+        println("tamaño 8")
+    } else {
+        println("exede el tamaño")
+    }
+
+//Bucles
+
+
+}
