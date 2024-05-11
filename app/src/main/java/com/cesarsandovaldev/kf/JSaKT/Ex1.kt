2254ed9fccef5c428ad2f5d3e6779608eb2f50fd
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-05-11 13:53:40 -0400

    add ejercicios convertidos de JavaScript a Kotlin

---
+++ app/src/main/java/com/cesarsandovaldev/kf/JSaKT/Ex1.kt
@@ -0,0 +1,16 @@
+package com.cesarsandovaldev.kf.JSaKT
+fun main() {
+val numeros = arrayOf(3,87,99,52,47,63,8,2)
+val valor = sumaPares(numeros)
+println(valor)
+}
+
+fun sumaPares (vuelta:Array<Int>):Int{
+    var suma: Int =0
+    for (i in vuelta){
+        if(i%2==0){
+            suma+=i
+        }
+    }
+    return suma
+}
